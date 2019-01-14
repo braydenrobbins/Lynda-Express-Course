43f17f9d5fd655e70fc90755f2b7f241515ef6ca
Brayden Robbins <braydenrobbins98@gmail.com>
2019-01-14 13:32:01 -0600

    finished the routes chapter on Lynda course

--- index.js
+++ index.js
@@ -13,26 +13,33 @@ app.get('/', (req, res) =>
     res.json(data)
 );
 
-app.get('/item/:id', (req, res) => {
+app.get('/item/:id', (req, res, next) => {
     console.log(req.params.id);
     let user = Number(req.params.id);
     console.log(user);
     console.log(data[user]);
     res.send(data[user]);
-});
-app.post('/newItem', (req, res) =>
-    res.send(`a post with /newItem route on port ${PORT}`)
+    next();
+}, (req, res) =>
+    console.log("Did you get the right data?")
 );
 
-app.put('/item', (req, res) =>
-    res.send(`a put with /item route on port ${PORT}`)
+
+app.route('/item')
+    .get((req, res) => {
+        //res.end()
+        //res.redirect("http://www.google.com")
+        //res.download("images/rocket.jpg")
+        res.send(`a get request with /item route on port ${PORT}`)
+    })
+    .put((req, res) =>
+        res.send(`a put with /item route on port ${PORT}`)
+    )
+    .delete((req, res) =>
+        res.send(`a delete with /item route on port ${PORT}`)
 );
 
-app.delete('/item', (req, res) =>
-    res.send(`a delete with /item route on port ${PORT}`)
-);
-
-app.listen(PORT, () =>
-    console.log(`Your servr is running on port ${PORT}`)
+app.listen(PORT, () =>{
+    console.log(`Your servr is running on port ${PORT}`);
     console.log(data)
-);
+});
